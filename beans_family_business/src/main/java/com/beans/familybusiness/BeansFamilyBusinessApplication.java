ae0f314b79baeab8c21c1b0e1962773d2f83554b
kyeon <kyeongbin3431@gmail.com>
2023-07-29 21:43:21 +0900

    수정사항

---
+++ beans_family_business/src/main/java/com/beans/familybusiness/BeansFamilyBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.beans.familybusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeansFamilyBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeansFamilyBusinessApplication.class, args);
+	}
+
+}
